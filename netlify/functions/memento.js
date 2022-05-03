f0535f4af005ee2c0128cec1442af2f3303f138b
Dan Visel <dbvisel@gmail.com>
2022-05-03 20:39:07 +0800

    now with error handling

--- netlify/functions/memento.js
+++ netlify/functions/memento.js
@@ -5,24 +5,33 @@ exports.handler = async (event, context) => {
   const theUrl = `http://web.archive.org/web/timemap/json/${url}`;
   // console.log(theUrl);
   let dates = [];
-  await fetch(theUrl, {
-    method: "GET",
-  })
-    .then((res) => res.json())
-    .then(async (r) => {
-      // console.log(r);
-      dates = r.slice(1).map((x) => x[1]);
-      // console.log(JSON.stringify(dates));
+  try {
+    await fetch(theUrl, {
+      method: "GET",
     })
-    .catch((e) => {
-      console.error("Error: ", e);
-      return {
-        statusCode: 500,
-        body: JSON.stringify([]),
-      };
-    });
-  return {
-    statusCode: 200,
-    body: JSON.stringify(dates),
-  };
+      .then((res) => res.json())
+      .then(async (r) => {
+        // console.log(r);
+        dates = r.slice(1).map((x) => x[1]);
+        // console.log(JSON.stringify(dates));
+      })
+      .catch((e) => {
+        console.error("Error: ", e);
+        return {
+          statusCode: 500,
+          body: JSON.stringify([]),
+        };
+      });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(dates),
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message,
+      }),
+    };
+  }
 };
